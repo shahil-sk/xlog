436de7ecf0f62158a73f9b0d3957bbe84c7afbf2
shahil-sk <shahil8ahmed.sk@gmail.com>
2024-12-02 12:54:49 +0530

    Initial Build

--- xPrint.py
+++ xPrint.py
@@ -1,19 +1,21 @@
 
 def xPrint(msg,flag=None): 
     if flag == "danger":
+        # Color Red
         print("\033[91m {}\033[00m" .format("[!] Danger  : ")+msg)
+
     elif flag == "warn":
+        # Color Yellow
         print("\033[93m {}\033[00m" .format("[!] Warning : ")+msg)
+
     elif flag == "success":
+        # Color Green
         print("\033[92m {}\033[00m" .format("[!] Success : ")+msg)
+
     elif flag == "note":
+        # Color Blue
         print("\033[94m {}\033[00m" .format("[!] Note    : ")+msg)
+
     else:
         print("\033[97m {}\033[00m" .format("[!] Log     : ")+msg)
  
-
-xPrint("Testing","danger")
-xPrint("Testing","warn")
-xPrint("Testing","success")
-xPrint("Testing","note")
-xPrint("Testing")
